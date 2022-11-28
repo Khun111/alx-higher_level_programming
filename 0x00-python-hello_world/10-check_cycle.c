1927793f1218cfcc5285f2fa905829d5cc763fc6
Oluwatobi <amure387@gmail.com>
2022-11-28 12:48:18 +0100

    push

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,8 +1,9 @@
 #include "lists.h"
+
 /**
- * check_cycle - checks for a cycle in a list
- * @list: list to check
- * Return: 0/1
+ * check_cycle - checks if a list has a cycle.
+ * @list: list to check.
+ * Return: 1/0
  */
 int check_cycle(listint_t *list)
 {
@@ -10,6 +11,7 @@ int check_cycle(listint_t *list)
 
 	if (!list || list->next == NULL)
 		return (0);
+
 	slow_p = list->next;
 	fast_p = list->next->next;
 
@@ -17,8 +19,10 @@ int check_cycle(listint_t *list)
 	{
 		if (slow_p == fast_p)
 			return (1);
+
 		slow_p = slow_p->next;
 		fast_p = fast_p->next->next;
 	}
+
 	return (0);
 }
