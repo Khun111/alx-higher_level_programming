0aa57a0d926de8856915b68e630ebafa8a4187a4
Oluwatobi <amure387@gmail.com>
2022-12-01 19:18:09 +0000

    push

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import hidden4
-printeds = dir(hidden4)
-for i in printeds:
-    if i[:2] != '__':
-        print(i)
+    import hidden_4
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
