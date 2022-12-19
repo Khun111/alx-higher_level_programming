c84f22906b95c5c42276eb4795be407a61b29bef
Oluwatobi <amure387@gmail.com>
2022-12-19 17:26:44 +0000

    doingnothing

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -1,21 +1,16 @@
 #include <Python.h>
 #include <stdio.h>
+void print_python_bytes(PyObject *p);
+void print_python_list(PyObject *p);
+void print_python_float(PyObject *p);
 
 /**
  * print_python_list - function to print
  * information about a Python list object
  * @p: Python Object
  */
-void print_python_list(PyObject *p)
+void print_python_list(PyObject *p __attribute__((unused)))
 {
-	if (!PyList_Check(p))
-	{
-		printf("Error: Object is not a Python list\n");
-		return;
-	}
-	printf("[*] Python list info\n");
-	printf("[*] Size of the Python List = %lu\n", PyList_Size(p));
-	printf("[*] Allocated = %lu\n", ((PyListObject *)p)->allocated);
 }
 
 /**
@@ -23,19 +18,8 @@ void print_python_list(PyObject *p)
  * information about a Python bytes object
  * @p: Python Object
  */
-void print_python_bytes(PyObject *p)
+void print_python_bytes(PyObject *p __attribute__((unused)))
 {
-	if (!PyBytes_Check(p))
-	{
-		printf("Error: Object is not a Python bytes object\n");
-		return;
-	}
-	printf("[*] Python bytes info\n");
-	printf("[*] Size of the Python bytes object = %lu\n", PyBytes_Size(p));
-	printf("[*] First %zu bytes:", PyBytes_Size(p) > 10 ? 10 : PyBytes_Size(p));
-	for (unsigned char i = 0; i < PyBytes_Size(p) && i < 10; i++)
-		printf(" %02x", (unsigned char)PyBytes_AsString(p)[i]);
-	printf("\n");
 }
 
 /**
@@ -43,14 +27,7 @@ void print_python_bytes(PyObject *p)
  * information about a Python float object
  * @p: Python Object
  */
-void print_python_float(PyObject *p)
+void print_python_float(PyObject *p __attribute__((unused)))
 {
-	if (!PyFloat_Check(p))
-	{
-		printf("Error: Object is not a Python float object\n");
-		return;
-	}
-	printf("[*] Python float info\n");
-	printf("[*] Float value: %.16g\n", PyFloat_AsDouble(p));
 }
 
