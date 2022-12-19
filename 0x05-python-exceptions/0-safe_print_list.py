988550cbcd5cd2756b88c235dd64fb04d120c985
Oluwatobi <amure387@gmail.com>
2022-12-19 10:52:53 +0100

    push

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,10 @@
+def safe_print_list(my_list=[], x=0):
+    l_len = 0
+    for i in range(x):
+        try:
+            print(f'{my_list[i]}', end='')
+            l_len += 1
+        except IndexError:
+            break
+    print()
+    return l_len
