9c12b75f8e526d1b15e61a356e81dc152f7dbdf5
Oluwatobi <amure387@gmail.com>
2022-12-26 20:40:34 +0100

    push

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+'''Module to print square of size size'''
 def print_square(size):
+	'''Function to  print square of size size'''
 	if not isinstance(size, int):
 		raise TypeError('size must be an integer')
 	if size < 0:
