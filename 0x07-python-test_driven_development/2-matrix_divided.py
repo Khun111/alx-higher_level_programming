1d7026ba58a7e8a895f317ab86e89d56e8803052
Oluwatobi <amure387@gmail.com>
2022-12-26 13:05:19 +0100

    push

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
 '''Module to divide a matrix contents'''
+
+
 def matrix_divided(matrix, div):
     '''Function we're using'''
-    if not [isinstance(num, (int, float)) for num in matrix]:
-        raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
+    if (
+        not isinstance(
+            matrix, list) or matrix == [] or matrix == [[]] or not all(
+            isinstance(
+                row, list) for row in matrix) or not all(
+                    isinstance(
+                        num, (int, float))) for num in [
+                            num for row in matrix for num in row]):
+        raise TypeError(
+            'matrix must be a matrix (list of lists) of integers/floats')
+    if [len(row) != len(matrix[0]) for row in matrix]
+        raise TypeError('Each row of the matrix must have the same size')
