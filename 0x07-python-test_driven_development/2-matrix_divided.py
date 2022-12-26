25d4969fb00766fdf42a9d48f838ff9a2420ff29
Oluwatobi <amure387@gmail.com>
2022-12-26 16:24:49 +0100

    push

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -16,3 +16,9 @@ def matrix_divided(matrix, div):
             'matrix must be a matrix (list of lists) of integers/floats')
     elif [len(row) != len(matrix[0]) for row in matrix]:
         raise TypeError('Each row of the matrix must have the same size')
+    if not isinstance(div, (int, float)):
+        raise TypeError('div must be a number')
+        if div == 0:
+            raise ZeroDivisionError('division by zero')
+        newlist = list(map(lambda x: x / div, row) for row in matrix)
+        return newlist
