9b23f6f9eeb033428f0ab4561899e2dc6a613215
Oluwatobi <amure387@gmail.com>
2022-12-27 07:09:01 +0100

    push

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -2,12 +2,9 @@
 def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError('text must be a string')
-    lines = text.split('\n')
-    for line in lines:
-        words = line.split()
-        for i, word in enumerate(words):
-            if word[-1] in ['.', ':', '?']:
-                print(word[:-1], end='\n\n')
-            else:
-                print(word, end=' ')
-        print()
+    for i in range(len(text)):
+        if text[i] == ' ':
+            continue
+        print(text[i], end='')
+        if text[i] in '., ?, :':
+            print('\n')
