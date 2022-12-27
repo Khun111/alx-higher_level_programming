9417aa81bd3bfd89b4a6fe3a9d39740f0f708392
Oluwatobi <amure387@gmail.com>
2022-12-27 10:59:53 +0100

    push

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -7,23 +7,26 @@ max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
     def test_max_integer(self):
-        # Test with a list of integers
+        '''Test with a list of integers'''
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
         self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
 
-        # Test with a list of strings
+        '''Test with a list of strings''' 
         self.assertEqual(max_integer(['a', 'b', 'c', 'd']), 'd')
         self.assertEqual(max_integer(['A', 'B', 'C', 'D']), 'D')
 
-        # Test with an empty list
+        '''Test with an empty list'''
         self.assertEqual(max_integer([]), None)
+        self.assertEqual(max_integer(''), None)
 
-        # Test with a list of mixed types
+        '''Test floats and floats mix'''
+        self.assertEqual(max_integer([1.0, 7, 7.5, -6, -9]), 7.5)
+        '''Test a list of mixed types''' 
         with self.assertRaises(TypeError):
             max_integer([1, 'a', 3, 4])
         with self.assertRaises(TypeError):
             max_integer([1, 2, 3, 'a'])
 
-        # Test with a list of length 1
+        '''Test with a list of length 1''' 
         self.assertEqual(max_integer([1]), 1)
         self.assertEqual(max_integer(['a']), 'a')
