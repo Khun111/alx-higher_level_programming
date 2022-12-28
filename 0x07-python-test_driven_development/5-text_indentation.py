7abafd3c811750685db0042a3194407428847cfa
Oluwatobi <amure387@gmail.com>
2022-12-28 10:34:25 +0100

    stash

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,10 +1,27 @@
 #!/usr/bin/python3
 def text_indentation(text):
-    if not isinstance(text, str):
-        raise TypeError('text must be a string')
-    for i in range(len(text)):
-        if text[i] == ' ':
-            continue
-        print(text[i], end='')
-        if text[i] in '., ?, :':
-            print('\n')
+    """This module prints a text with 2 new lines after each of these
+    characters: ., ? and :
+    Args:
+        text (str): string to print
+    Raises:
+        TypeError: test must be a string
+    """
+    if (type(text) is not str):
+        raise TypeError("text must be a string")
+
+    i = 0
+    for _ in range(len(text)):
+        if (i <= len(text) - 1):
+            print(text[i], end="")
+            if (text[i] == "." or text[i] == "?" or text[i] == ":"):
+                print()
+                print()
+                if (i < len(text) - 1 and text[i + 1] == " "):
+                    i += 1
+                    while (text[i] == " "):
+                        i += 1
+                else:
+                    i += 1
+            else:
+                i += 1
