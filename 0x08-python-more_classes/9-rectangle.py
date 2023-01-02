42170f5bd356831aad439b72da4f42d48f9fc223
Oluwatobi <amure387@gmail.com>
2023-01-02 14:03:32 +0100

    push

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -31,9 +31,9 @@ class Rectangle:
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
-            raise TypeError('width must be an integer')
+            raise TypeError('height must be an integer')
         if value < 0:
-            raise ValueError('width must be >= 0')
+            raise ValueError('height must be >= 0')
         self.__height = value
 
     def area(self):
@@ -57,7 +57,7 @@ class Rectangle:
 
     def __repr__(self):
         shapeinst = 'Rectangle(' + str(self.__width) + \
-            ' , ' + str(self.__height) + ')'
+            ', ' + str(self.__height) + ')'
         return shapeinst
 
     def __del__(self):
