1975e0c6728fb3a5f430d10228e66cf2ce8d6478
Oluwatobi <amure387@gmail.com>
2023-01-03 09:47:00 +0100

    push

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,6 @@
+def magic_string():
+    magic_string.count += 1
+    return "BestSchool, " * (magic_string.count - 1) + "BestSchool"
+
+magic_string.count = 0
+
--- 0x09-python-everything_is_object/main.py
+++ 0x09-python-everything_is_object/main.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-def assign_value(n, v):
-    n = v
+LockedClass = __import__('101-locked_class').LockedClass
 
-l1 = [1, 2, 3]
-l2 = [4, 5, 6]
-assign_value(l1, l2)
-print(l1)
+lc = LockedClass()
+lc.first_name = "John"
+try:
+    lc.last_name = "Snow"
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
