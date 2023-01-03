42ab0c5bfceacdb8d0432a6c16be0234725d1247
Oluwatobi <amure387@gmail.com>
2023-01-03 17:19:22 +0100

    push

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -6,8 +6,5 @@ def magic_string():
     '''magic_string() that returns a string
     “BestSchool” n times the numberof the iteration
     '''
-    magic_string.count += 1
+    magic_string.count = getattr(magic_string, 'count', 0) + 1
     return "BestSchool, " * (magic_string.count - 1) + "BestSchool"
-
-
-magic_string.count = 0
