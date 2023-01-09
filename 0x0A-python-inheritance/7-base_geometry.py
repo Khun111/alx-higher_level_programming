0d86c9dd4d7ea7053e03ca01a857774634425a34
Oluwatobi <amure387@gmail.com>
2023-01-09 23:48:27 +0100

    push

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -10,7 +10,7 @@ class BaseGeometry:
         raise Exception('area() is not implemented')
 
     def integer_validator(self, name, value):
-        '''Function to calidate value entered'''
+        '''Function to validate value entered'''
         if type(value) != int:
             raise TypeError(f'{name} must be an integer')
         if value <= 0:
