f6fe796564d0e45b42c7f634a5bb28f48cf262e0
Oluwatobi <amure387@gmail.com>
2023-01-16 18:34:31 +0100

    work

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,7 +2,7 @@
 '''This Module creates the Base class to handle ids'''
 import json
 import csv
-# import turtle
+import turtle
 
 
 class Base:
@@ -103,8 +103,6 @@ the JSON string representation of list_objs to a file'''
         except IOError:
             return []
 
-
-'''
     @staticmethod
     def draw(list_rectangles, list_squares):
         turtle.setup(width=800, height=600)
@@ -134,4 +132,3 @@ the JSON string representation of list_objs to a file'''
                 turtle.right(90)
 
         turtle.exitonclick()
-'''
