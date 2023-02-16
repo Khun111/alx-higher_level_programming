a7715811d9e93a79988b8497ded3e8f38fb5a7c3
Oluwatobi <amure387@gmail.com>
2023-02-16 03:53:10 +0100

    updated

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,11 +1,9 @@
 -- Lists all genres in the database hbtn_0d_tvshows_rate by their rating.
--- Records are ordered by descending rating.
 SELECT `name`, SUM(`rate`) AS `rating`
-  FROM `tv_genres` AS g
-       INNER JOIN `tv_show_genres` AS s
-       ON s.`genre_id` = g.`id`
-
+  FROM `tv_genres` AS tg
+       INNER JOIN `tv_show_genres` AS ts
+       ON ts.`genre_id` = tg.`id`
        INNER JOIN `tv_show_ratings` AS r
-       ON r.`show_id` = s.`show_id`
+       ON r.`show_id` = ts.`show_id`
  GROUP BY `name`
  ORDER BY `rating` DESC;
