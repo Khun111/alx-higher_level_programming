19ac7896b69bd6df3340734099cb93972cdaf227
Oluwatobi <amure387@gmail.com>
2023-02-16 03:39:44 +0100

    updated 16,12,14, 15

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,11 +1,2 @@
--- Lists all shows and genres linked to the show from the
--- database hbtn_0d_tvshows.
--- Records are ordered by ascending show title and genre name.
-SELECT t.`title`, g.`name`
-  FROM `tv_shows` AS t
-       LEFT JOIN `tv_show_genres` AS s
-       ON t.`id` = s.`show_id`
-
-       LEFT JOIN `tv_genres` AS g
-       ON s.`genre_id` = g.`id`
- ORDER BY t.`title`, g.`name`;
+-- Lists all shows and genres linked to the show from the database hbtn_0d_tvshows.
+SELECT t.`title`, tg.`name` FROM `tv_shows` AS t LEFT JOIN `tv_show_genres` AS ts ON t.`id` = ts.`show_id` LEFT JOIN `tv_genres` AS tg ON s.`genre_id` = tg.`id` ORDER BY t.`title`, tg.`name`;
