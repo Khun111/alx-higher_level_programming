098ddf6b73e1b1c42aea76357a64583b4fac1144
Oluwatobi <amure387@gmail.com>
2023-03-13 22:39:48 +0100

    fix: task 6, 7, 8

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,4 +1,3 @@
 #!/usr/bin/node
 let i = 0;
-while (i++ < process.argv[2])
-	console.log('C is fun');
+while ((i++ < process.argv[2])) console.log('C is fun');
--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 let arg = process.argv[2], i = 0, j = 0;
-if (isNaN(arg))
+if ((isNaN(arg)))
 	console.log('Missing size');
-while (i++ < arg)
+while ((i++ < arg))
 {
-	while (j++ < arg)
+	while ((j++ < arg))
 	{
 		process.stdout.write('X');
 	}
