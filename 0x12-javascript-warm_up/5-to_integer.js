10f327d914e4a7a1f1f652cf9564b1dc350beb60
Oluwatobi <amure387@gmail.com>
2023-03-13 22:32:32 +0100

    fix:

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,5 +1,3 @@
 #!/usr/bin/node
-if (Number(process.argv[2]))
-	console.log('My number: ' + Math.floor(process.argv[2]));
-else
-	console.log('Not a number')
+if ((Number(process.argv[2]))) console.log('My number: ' + Math.floor(process.argv[2]));
+else console.log('Not a number');
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
 let langArray = ['C is fun', 'Python is cool', 'JavaScript is amazing']
 let i = -1;
-while (i++ < langArray.length - 1)
-	console.log(langArray[i]);
+while (i++ < langArray.length - 1) console.log(langArray[i]);
