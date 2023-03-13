6dca96a95e27e056ead70a2abbfd02573697b95e
Oluwatobi <amure387@gmail.com>
2023-03-13 23:03:05 +0100

    fix: rest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
-let i = 0, max = 0, arr = process.argv.slice(2);
+let arr = process.argv.slice(2);
 
-if (process.argv.length <= 3)
-	console.log(0);
+if (process.argv.length <= 3) console.log(0);
 else
 {
 	const sortArr = arr.sort((a, b) => b - a)
--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,7 +3,7 @@ let arg = process.argv[2], i = 0, j = 0;
 if ((isNaN(arg))) console.log('Missing size');
 while ((i++ < arg))
 {
-	while ((j++ < arg)) process.stdout.write('X');
-	j = 0;
-	process.stdout.write('\n');
+  while ((j++ < arg)) process.stdout.write('X');
+  j = 0;
+  process.stdout.write('\n');
 }
