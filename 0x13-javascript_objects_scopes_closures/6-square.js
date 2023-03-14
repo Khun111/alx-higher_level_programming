5ed4601a2c35b188e3548770c508d7107a3ad0c6
Oluwatobi <amure387@gmail.com>
2023-03-14 11:13:06 -0700

    feat: semistandard style

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,13 +3,13 @@ const Rectangle = require('./4-rectangle');
 
 class Square extends Rectangle {
   constructor (size) {
-    super (size, size)
+    super(size, size);
   }
 
-  charPrint(c) {
-  let i = 0;
-  if ((c === undefined)) c = 'X';
-  while ((i++ < this.width)) console.log(c.repeat(this.height));
+  charPrint (c) {
+    let i = 0;
+    if ((c === undefined)) c = 'X';
+    while ((i++ < this.width)) console.log(c.repeat(this.height));
   }
 }
 module.exports = Square;
--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  let i = list.length, revArray = [];
+  let i = list.length;
+  const revArray = [];
   while ((i-- > 0)) revArray.push(list[i]);
   return revArray;
-}
+};
