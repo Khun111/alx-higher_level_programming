7f16dc97b9b60ea2040d704a06291e2b8739d672
Oluwatobi <amure387@gmail.com>
2023-03-17 20:37:30 +0000

    feat: done task 0 and passed pycodestyle

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''Module for listing states'''
+import sys
+import MySQLdb
+con = MySQLdb.connect(
+    host='localhost', port=3306,
+    user=sys.argv[1], passwd=sys.argv[2],
+    db=sys.argv[3])
+cur = con.cursor()
+query = 'SELECT * FROM states'
+cur.execute(query)
+lists = cur.fetchall()
+for row in lists:
+    print(row)
