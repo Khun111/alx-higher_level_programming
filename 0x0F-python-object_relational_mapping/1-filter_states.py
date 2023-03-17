c13e69d601dc892a7a8c8357ff841783bdfdf471
Oluwatobi <amure387@gmail.com>
2023-03-17 20:59:05 +0000

    added readme

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -2,9 +2,12 @@
 '''Module for listing states'''
 import sys
 import MySQLdb
-con = MySQLdb.connect(host='localhost', port=3306,user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+con = MySQLdb.connect(
+    host='localhost', port=3306,
+    user=sys.argv[1], passwd=sys.argv[2],
+    db=sys.argv[3])
 cur = con.cursor()
-query = 'SELECT "N%" FROM states'
+query = 'SELECT * FROM states WHERE states.name LIKE "N%"'
 cur.execute(query)
 lists = cur.fetchall()
 for row in lists:
