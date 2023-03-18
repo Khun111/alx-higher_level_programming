225fb9846f6796738e004e16ceb9092abb7c0219
Oluwatobi <amure387@gmail.com>
2023-03-18 05:23:16 +0000

    feat: added main clause

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -2,13 +2,14 @@
 '''Module for listing states'''
 import sys
 import MySQLdb
-con = MySQLdb.connect(
-    host='localhost', port=3306,
-    user=sys.argv[1], passwd=sys.argv[2],
-    db=sys.argv[3])
-cur = con.cursor()
-query = 'SELECT * FROM states WHERE states.name LIKE "N%"'
-cur.execute(query)
-lists = cur.fetchall()
-for row in lists:
-    print(row)
+if __name__ == '__main__':
+    con = MySQLdb.connect(
+        host='localhost', port=3306,
+        user=sys.argv[1], passwd=sys.argv[2],
+        db=sys.argv[3])
+    cur = con.cursor()
+    query = 'SELECT * FROM states WHERE name LIKE "N%"'
+    cur.execute(query)
+    lists = cur.fetchall()
+    for row in lists:
+        print(row)
