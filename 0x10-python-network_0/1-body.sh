210e63aea361214bb483d47aa3b369b3ad7143dd
Oluwatobi <amure387@gmail.com>
2023-03-30 16:51:47 +0000

    feat: fixed task 0 and did task 1'

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 # Takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -w '%{size_download}\n' -o /dev/null -s $1
+RES=$(curl -w '%{http_code}' -o /dev/null -s $1)
+if [ $RES == 200 ]; then
+  curl -s $1
+fi
