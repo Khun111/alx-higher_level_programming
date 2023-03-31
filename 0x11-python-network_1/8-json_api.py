322ffe6c179eb37d9b37e6d8f15779768c443b2c
Oluwatobi <amure387@gmail.com>
2023-03-31 21:33:20 +0000

    feat: passing data plus checking json format of response

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     res = requests.post(url, data=q_data)
     try:
         res = res.json()
-        print(f'[{res.get("id")}], i\
+        print(f'[{res.get("id")}] \
 {res.get("name")}') if res else print('No result')
     except ValueError:
         print('Not a valid JSON')
