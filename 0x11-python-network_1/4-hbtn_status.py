ee6b795769ffe466bbccc1ec6d112b5af09f600f
Oluwatobi <amure387@gmail.com>
2023-03-31 20:07:29 +0000

    feat: fetch with requests

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,7 +2,7 @@
 '''Fetch with requests'''
 import requests
 url = 'https://alx-intranet.hbtn.io/status'
-response = requests.get(url).json()
+response = requests.get(url)
 print('Body response:')
 print(f'\t- type: {type(response)}')
 print(f'\t- content: {response}')
