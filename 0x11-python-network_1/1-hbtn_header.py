efe19e9ec724d855cf418f0e2af98f87fa8f2180
Oluwatobi <amure387@gmail.com>
2023-03-31 18:58:03 +0000

    feat : post request with email data

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,5 +6,5 @@ if __name__ == '__main__':
     url = sys.argv[1]
     flex_req = urllib.request.Request(url)
     with urllib.request.urlopen(flex_req) as res:
-        response = res.getheader('X-Request-Id')
+        response = res.info().get('X-Request-Id')
         print(response)
--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 import urllib.request
+import urllib.parse
 import sys
 '''Sends POST request with email data'''
 if __name__ == '__main__':
     url = sys.argv[1]
     user_mail = sys.argv[2]
-    e_data = urllib.parse.urlencode({'email': user_mail}).encode('utf-8')
+    e_data = urllib.parse.urlencode({'email': user_mail}).encode('ascii')
     with urllib.request.urlopen(url, e_data) as res:
         response = res.read().decode('utf-8')
         print(response)
