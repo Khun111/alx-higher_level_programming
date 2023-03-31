c2b7b62789794bc1ca0cc42c32dc60761fc16ce3
Oluwatobi <amure387@gmail.com>
2023-03-31 22:23:08 +0000

    feat: github id via auth

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -6,6 +6,6 @@ if __name__ == '__main__':
     name = sys.argv[1]
     pat = sys.argv[2]
     url = 'https://api.github.com/user'
-    res = requests.post(url, auth=(name, pat))
+    res = requests.get(url, auth=(name, pat))
     print(res.json().get('\
 id')) if res.status_code == 200 else print('None')
