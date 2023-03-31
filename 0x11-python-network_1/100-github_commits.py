511f2da3b9c09f09c0012811e87efa4fefa221bd
Oluwatobi <amure387@gmail.com>
2023-03-31 23:14:18 +0000

    feat: get last 10 commits

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''Display github id using auth'''
+import requests
+import sys
+if __name__ == '__main__':
+    user = sys.argv[1]
+    repo = sys.argv[2]
+    url = f'https://api.github.com/repos/{user}/{repo}/commits'
+    res = requests.get(url)
+    res = res.json()
+    for details in res[:10]:
+        sha = res['sha']
+        author = res['commit']['author']['name']
+        print(sha + ': ' + author)
