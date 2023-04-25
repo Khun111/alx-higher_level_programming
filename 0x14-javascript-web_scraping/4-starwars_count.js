917fe4b15658da641805d3880c84c13c1c8f3fd3
Oluwatobi <amure387@gmail.com>
2023-04-25 21:11:13 +0000

    fix: task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -6,7 +6,7 @@ request.get(process.argv[2], (error, response, body) => {
   if (error) console.error(error);
   const data = JSON.parse(body).results;
   data.forEach((movie) => {
-    if (movie.characters.some((url) => url === castUrl)) count++;
+    if (movie.characters.includes(castUrl)) count++;
   }
   );
   console.log(count);
