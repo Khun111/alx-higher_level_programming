0aff4dca35aaf887e204cb9a6c5f5fedfa674eb0
Oluwatobi <amure387@gmail.com>
2023-04-25 17:00:47 +0000

    fix: task 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 const fileSys = require('fs');
 
-fileSys.writeFile(process.argv[2], process.argv[3], 'utf-8', (err) => { err ? console.error(err) : console.log('Success!'); });
+fileSys.writeFile(process.argv[2], process.argv[3], 'utf-8', (err) => { err ? console.error(err) : undefined; });
