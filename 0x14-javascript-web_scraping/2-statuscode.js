900df610c22df36b603771156f3b20de73c99337
Oluwatobi <amure387@gmail.com>
2023-04-25 14:00:08 +0000

    fix: semistandard style

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 const request = require('request');
-request.get(process.argv[2], (error, response, body) => { error ? console.error(error) : console.log(`code: ${response.statusCode}`)});
+request.get(process.argv[2], (error, response, body) => { error ? console.error(error) : console.log(`code: ${response.statusCode}`); });
