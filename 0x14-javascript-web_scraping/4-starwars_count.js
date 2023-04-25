e3e49491242888650ff4a36479bd70af15d8f8c9
Oluwatobi <amure387@gmail.com>
2023-04-25 22:41:05 +0000

    fix: task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,12 +1,14 @@
 #!/usr/bin/node
 const request = require('request');
-const castUrl = 'https://swapi-api.alx-tools.com/api/people/18/';
+const hisId = '18'
 let count = 0;
 request.get(process.argv[2], (error, response, body) => {
   if (error) console.error(error);
   const data = JSON.parse(body).results;
-  data.forEach((movie) => {
-    if (movie.characters.includes(castUrl)) count++;
+  data.forEach((movies) => {
+    movies.characters.forEach((movie) => {
+    if (movie.includes(hisId)) count++;
+});
   }
   );
   console.log(count);
