55e8898bc96c3b97c90c8e33eb2c1e0a0f2c246a
Oluwatobi <amure387@gmail.com>
2023-04-25 22:45:38 +0000

    fix: semistandard

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 const request = require('request');
-const hisId = '18'
+const hisId = '18';
 let count = 0;
 request.get(process.argv[2], (error, response, body) => {
   if (error) console.error(error);
   const data = JSON.parse(body).results;
   data.forEach((movies) => {
     movies.characters.forEach((movie) => {
-    if (movie.includes(hisId)) count++;
-});
+      if (movie.includes(hisId)) count++;
+    });
   }
   );
   console.log(count);
