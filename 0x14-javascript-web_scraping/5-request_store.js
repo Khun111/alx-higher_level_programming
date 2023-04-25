0772284df044e302e42d345588e22696514d67e0
Oluwatobi <amure387@gmail.com>
2023-04-25 17:13:17 +0000

    feat: added task 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -2,6 +2,9 @@
 const request = require('request');
 const fileSys = require('fs');
 
-request.get(process.argv[2], (error, response, body) => { error && response.statusCode !== 200 ? console.error(error) : data = JSON.parse(body); });
-
-fileSys.writeFile(process.argv[3], data, 'utf-8', (err) => { err ? console.error(err) : console.log('Successful'); });
+request.get(process.argv[2], (error, response, body) => {
+        if (error && response.statusCode !== 200 ) {
+        console.error(error)
+        } else {
+        fileSys.writeFile(process.argv[3], body, 'utf-8', (err) => { err ? console.error(err) : undefined; });
+        }});
